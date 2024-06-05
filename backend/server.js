af3f74b59719366dcf01936cbc3d21f33d6b8f07
MOHD SAMIULLAH <samiullah0813@gmail.com>
2024-06-05 18:48:03 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.get("/", (req,res)=> {
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
